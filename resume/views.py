cf10e8a77cd12ac61b0f38ab3e8f3b1ac395bcb4
Ehsan Lak <151562034+Ehsunpy@users.noreply.github.com>
2025-02-11 23:49:35 +0330

    Delete resume directory

--- resume/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-# Create your views here.
-
-class Resume(TemplateView):
-    template_name='resume/resume.html'
